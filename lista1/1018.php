7a9cc4e38a8cf4170448c239cabebf59f1ad0573
bonfieti <bonfietianajulia@gmail.com>
2024-03-26 09:10:53 -0300

    lista1

---
+++ lista1/1018.php
@@ -0,0 +1,15 @@
+<?php
+
+$val = intval(readline());
+$cedulas = [100, 50, 20, 10, 5, 2, 1];
+
+echo $val . "\n";
+
+foreach ($cedulas as $ced) {
+    $qtd_n = intval($val / $ced);
+    $val %= $ced;
+    $ced = number_format($ced, 2, ',', ''); 
+
+    echo $qtd_n . " nota(s) de R$ $ced\n";
+}
+?>
